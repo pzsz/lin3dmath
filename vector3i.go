11bee198f51dae223f31a8be48c911283729e7f5
Przemyslaw Szczepaniak <przeszczep@gmail.com>
2012-04-03 21:14:16 +0100

    Added some vector3i operations

--- vector3i.go
+++ vector3i.go
@@ -8,6 +8,18 @@ func (s Vector3i) Add(o Vector3i) Vector3i {
 	return Vector3i{s.X + o.X, s.Y + o.Y, s.Z + o.Z}
 }
 
+func (s *Vector3i) AddIP(o Vector3i) {
+	s.X += o.X; s.Y += o.Y; s.Z += o.Z;
+}
+
+func (s Vector3i) MulI(o int) Vector3i {
+	return Vector3i{s.X*o, s.Y*o, s.Z*o}
+}
+
+func (s Vector3i) Mul3I(o Vector3i) Vector3i {
+	return Vector3i{s.X*o.X, s.Y*o.Y, s.Z*o.Z}
+}
+
 func (s Vector3i) ToF() Vector3f {
 	return Vector3f{float32(s.X),
 		float32(s.Y),
